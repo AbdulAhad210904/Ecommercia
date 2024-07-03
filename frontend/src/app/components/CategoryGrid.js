f87b555ab674aadaa585cdf56c6eb34d817a8cfa
Abdul-Ahad-Ali <123318968+AbdulAhad210904@users.noreply.github.com>
2024-07-03 09:54:16 +0500

    updated Category and Product Grid UI

--- frontend/src/app/components/CategoryGrid.js
+++ frontend/src/app/components/CategoryGrid.js
@@ -1,6 +1,5 @@
 "use client";
 import React from 'react';
-import Link from 'next/link'; // Import Link from next/link for client-side navigation
 
 const CategoryGrid = ({ title, categories, onClick }) => {
   return (
--- frontend/src/app/components/ProductGrid.js
+++ frontend/src/app/components/ProductGrid.js
@@ -3,35 +3,29 @@ import React from 'react';
 
 const ProductGrid = ({ title, products }) => {
   return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">{title}</h2>
-
-        
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {products?.map(product => (
-            <div key={product.id} className="group relative">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                <img
-                  src={product.images[0]}
-                  alt={product.title}
-                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                />
-              </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <a href="#">
-                      <span aria-hidden="true" className="absolute inset-0"></span>
-                      {product.title}
-                    </a>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+    <div className="bg-gray-100">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900">{title}</h2>
+          <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+            {products?.map((product) => (
+              <div key={product.id} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                  <img className="p-8 rounded-t-lg" src={product.images[0]} alt={product.title} />
+                </a>
+                <div className="px-5 pb-5">
+                  <a href="#">
+                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{product.title}</h5>
+                  </a>
+                  <div className="flex items-center mt-2.5 mb-5"></div>
+                  <div className="flex items-center justify-between">
+                    <span className="text-3xl font-bold text-gray-900 dark:text-white">${product.price}</span>
+                    <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                  </div>
                 </div>
-                <p className="text-sm font-medium text-gray-900">${product.price}</p>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
