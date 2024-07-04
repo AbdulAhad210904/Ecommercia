292558a004d5f4117874183aa7f002beedcee2ae
Abdul-Ahad-Ali <123318968+AbdulAhad210904@users.noreply.github.com>
2024-07-04 12:50:43 +0500

    Cart of User Page Configured

--- frontend/src/app/redux/reducers/cartReducer.js
+++ frontend/src/app/redux/reducers/cartReducer.js
@@ -1,4 +1,13 @@
-import { ADD_TO_CART_SUCCESS, ADD_TO_CART_FAILURE } from '../actions/cartActions';
+import {
+  ADD_TO_CART_SUCCESS,
+  ADD_TO_CART_FAILURE,
+  FETCH_CART_SUCCESS,
+  FETCH_CART_FAILURE,
+  UPDATE_CART_QUANTITY_SUCCESS,
+  UPDATE_CART_QUANTITY_FAILURE,
+  DELETE_CART_ITEM_SUCCESS,
+  DELETE_CART_ITEM_FAILURE,
+} from '../actions/cartActions';
 
 const initialState = {
   cartItems: [],
@@ -8,12 +17,18 @@ const initialState = {
 const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_CART_SUCCESS:
+    case FETCH_CART_SUCCESS:
+    case UPDATE_CART_QUANTITY_SUCCESS:
+    case DELETE_CART_ITEM_SUCCESS:
       return {
         ...state,
-        cartItems: action.payload.items,
+        cartItems: action.payload,
         error: null,
       };
     case ADD_TO_CART_FAILURE:
+    case FETCH_CART_FAILURE:
+    case UPDATE_CART_QUANTITY_FAILURE:
+    case DELETE_CART_ITEM_FAILURE:
       return {
         ...state,
         error: action.payload,
