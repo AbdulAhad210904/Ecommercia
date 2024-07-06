8e499e275f0b79f5f520d0d3a3af8e1ddafc2c01
Abdul-Ahad-Ali <123318968+AbdulAhad210904@users.noreply.github.com>
2024-07-06 16:19:02 +0500

    Payment Gateway UI Done

--- frontend/src/app/result/page.jsx
+++ frontend/src/app/result/page.jsx
@@ -1,8 +1,9 @@
 'use client'
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Link from 'next/link';
 
-const ResultPage = ({ searchParams}) => {
+const ResultPage = ({ searchParams }) => {
   const session_id = searchParams.session_id;
   const [session, setSession] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -26,27 +27,36 @@ const ResultPage = ({ searchParams}) => {
   }, [session_id]);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <div className="flex items-center justify-center h-screen">Loading...</div>;
   }
 
   if (!session) {
-    return <div>Session not found</div>;
+    return <div className="flex items-center justify-center h-screen">Session not found</div>;
   }
 
   return (
-    <div>
-      <h1>Payment Result</h1>
-      {session.payment_status === 'paid' ? (
-        <div>
-          <p>Thank you for your donation!</p>
-          <p>Payment ID: {session.id}</p>
-          <p>Amount: ${(session.amount_total / 100).toFixed(2)} {session.currency.toUpperCase()}</p>
+    <div className="flex items-center justify-center min-h-screen bg-gray-50 dark:bg-gray-900">
+      <div className="max-w-lg p-8 bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden">
+        <h1 className="text-2xl font-bold text-gray-900 dark:text-white mb-4">Payment Result</h1>
+        {session.payment_status === 'paid' ? (
+          <div>
+            <p className="text-green-600 dark:text-green-400 mb-2">Thank you for Shopping!</p>
+            <p className="text-gray-700 dark:text-gray-300 mb-2">Payment ID:</p>
+            <p className="text-gray-700 dark:text-gray-300 overflow-hidden overflow-ellipsis">{session.id}</p>
+            <p className="text-gray-700 dark:text-gray-300 mb-2">Amount: ${(session.amount_total / 100).toFixed(2)} {session.currency.toUpperCase()}</p>
+          </div>
+        ) : (
+          <div>
+            <p className="text-red-600 dark:text-red-400">Payment failed or was canceled.</p>
+          </div>
+        )}
+
+        <div className="mt-4">
+            <a  href="/" className="text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white transition duration-300 ease-in-out">
+              Back to Main Page
+            </a>
         </div>
-      ) : (
-        <div>
-          <p>Payment failed or was canceled.</p>
-        </div>
-      )}
+      </div>
     </div>
   );
 };
