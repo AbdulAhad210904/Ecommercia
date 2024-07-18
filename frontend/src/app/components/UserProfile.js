d3efe2027f1c7d4ab008e9dcc1a32498917aff35
Abdul-Ahad-Ali <123318968+AbdulAhad210904@users.noreply.github.com>
2024-07-18 11:30:10 +0500

    Navbar Hamburger configured for mobile

--- frontend/src/app/components/UserProfile.js
+++ frontend/src/app/components/UserProfile.js
@@ -1,9 +1,18 @@
-// Import React
-import React from 'react';
+"use client";
+import React, { useState } from 'react';
+import UpdateProfileModal from './UpdateProfileModal';
 
-// Define UserProfile component
 const UserProfile = ({ user }) => {
-    console.log(user);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="bg-white shadow-md rounded-lg p-4">
       <h2 className="text-xl font-bold mb-4">User Profile</h2>
@@ -19,6 +28,16 @@ const UserProfile = ({ user }) => {
         <p className="font-semibold">Email:</p>
         <p>{user.email}</p>
       </div>
+      <button
+        className="mt-4 bg-blue-500 text-white py-2 px-4 rounded"
+        onClick={handleOpenModal}
+      >
+        Update Profile
+      </button>
+
+      {isModalOpen && (
+        <UpdateProfileModal user={user} onClose={handleCloseModal} />
+      )}
     </div>
   );
 };
