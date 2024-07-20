c53c93bb4de44159915fc5dc644f42103fa44c9b
Abdul-Ahad-Ali <123318968+AbdulAhad210904@users.noreply.github.com>
2024-07-20 12:11:19 +0500

    Dark & Light mode confgured for login & Signup Page

--- frontend/src/app/signup/page.js
+++ frontend/src/app/signup/page.js
@@ -75,9 +75,13 @@ const Signup = () => {
                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                 <input type="password" name="password" id="password" value={formData.password} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="••••••••" required />
               </div>
-              <button type="submit" className="w-full bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center text-white border border-primary-600 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 dark:border-primary-600">
+              <button
+  type="submit"
+  className="w-full bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center text-white border border-blue-600 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:border-blue-600"
+>
   Create an account
 </button>
+
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Already have an account? <Link href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
               </p>
